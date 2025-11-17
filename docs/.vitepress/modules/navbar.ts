d0632c6c9f6ac5393d1e7648b14c4e00338afe41
Desktop <user@desktop>
2025-11-17 13:19:02 -0300

    fix/navigation

--- docs/.vitepress/modules/navbar.ts
+++ docs/.vitepress/modules/navbar.ts
@@ -1,8 +1,3 @@
 export default [
 	{ text: 'Home', link: '/' },
-	{ text: 'Articles', link: '/articles', activeMatch: '/articles/*' },
-	{ text: 'Books', link: '/books', activeMatch: '/books/*' },
-	{ text: 'Essays', link: '/essays', activeMatch: '/essays/*' },
-	{ text: 'Studies', link: '/studies', activeMatch: '/studies/*' },
-	{ text: 'University', link: '/university', activeMatch: '/university/*' },
 ]

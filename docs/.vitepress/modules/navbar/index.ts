17efc2f8c5dd67158468cc50e6af4ebff80fbf89
Desktop <user@desktop>
2026-02-17 12:08:53 -0300

    feature/update

--- docs/.vitepress/modules/navbar/index.ts
+++ docs/.vitepress/modules/navbar/index.ts
@@ -3,24 +3,4 @@ export default [
 		link: '/',
 		text: 'Home'
 	},
-	{
-		activeMatch: '/learning/',
-		link: '/learning/',
-		text: 'Learning'
-	},
-	{
-		activeMatch: '/literature/',
-		link: '/literature/',
-		text: 'Literature'
-	},
-	{
-		activeMatch: '/notes/',
-		link: '/notes/',
-		text: 'Notes'
-	},
-	{
-		activeMatch: '/research/',
-		link: '/research/',
-		text: 'Research'
-	},
 ]
